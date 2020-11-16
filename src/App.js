215440675ac76b936bcf41eff7477a1a20a7d093
Dylan Sleith <sleithdylan@gmail.com>
2020-11-16 16:12:35 +0000

    Connect to database

---
+++ src/App.js
@@ -0,0 +1,48 @@
+// Imports React, Component
+import React, { Component } from 'react';
+// Imports App CSS file
+import './App.css';
+// Imports Content Component
+import { Content } from './components/Content';
+// Imports Create Component
+import { Create } from './components/Create';
+// Imports Read Component
+import { Read } from './components/Read';
+// Imports Bootstrap CSS file
+import 'bootstrap/dist/css/bootstrap.min.css';
+// Imports Navbar, Nav Components from React Bootstrap
+import { Navbar, Nav } from 'react-bootstrap';
+
+// Imports Router, Switch, Route from React Router
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+// App Component
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className='App'>
+          {/* Navbar */}
+          <Navbar bg='primary' variant='dark'>
+            <Navbar.Brand href='/'>Navbar</Navbar.Brand>
+            <Nav className='mr-auto'>
+              <Nav.Link href='/'>Home</Nav.Link>
+              <Nav.Link href='/read'>Read</Nav.Link>
+              <Nav.Link href='/create'>Create</Nav.Link>
+            </Nav>
+          </Navbar>
+          <br />
+          {/* Renders the first child <Route> that matches location */}
+          <Switch>
+            <Route path='/' component={Content} exact />
+            <Route path='/create' component={Create} exact />
+            <Route path='/read' component={Read} exact />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
+
+// Exports App Component
+export default App;
